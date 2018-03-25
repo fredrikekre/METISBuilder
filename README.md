4413ab366fc1fb24274457d0684902567323f2d0
fredrikekre <fredrik.ekre@chalmers.se>
2018-03-25 22:24:24 +0200

    Add autogenerated files to build metis

---
+++ README.md
@@ -0,0 +1,9 @@
+# METISBuilder
+
+[![Build Status](https://travis-ci.org/fredrikekre/METISBuilder.svg?branch=master)](https://travis-ci.org/fredrikekre/METISBuilder)
+
+This repository builds binary artifacts for the metis project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/fredrikekre/METISBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,44 @@
+using BinaryBuilder
+
+# Collection of sources required to build metis
+sources = [
+    "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz" =>
+    "76faebe03f6c963127dbb73c13eab58c9a3faeae48779f049066a21c087c5db2",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd metis-5.1.0/
+make config prefix=$prefix shared=1
+make
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    BinaryProvider.Linux(:i686, :glibc),
+    BinaryProvider.Linux(:x86_64, :glibc),
+    BinaryProvider.Linux(:aarch64, :glibc),
+    BinaryProvider.Linux(:armv7l, :glibc),
+    BinaryProvider.Linux(:powerpc64le, :glibc),
+    BinaryProvider.MacOS()
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libmetis", :libmetis)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "metis", sources, script, platforms, products, dependencies)
+
