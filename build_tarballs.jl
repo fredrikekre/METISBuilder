19061e243861cfe27151a854b1cddb5365a68b2c
Fredrik Ekre <ekrefredrik@gmail.com>
2019-04-03 00:51:16 +0200

    Fix build on mingw by adding patches from https://github.com/msys2/MINGW-packages/tree/master/mingw-w64-metis.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,12 +9,19 @@ version = v"5.1.0"
 sources = [
     "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz" =>
     "76faebe03f6c963127dbb73c13eab58c9a3faeae48779f049066a21c087c5db2",
-
+    "./bundled",
 ]
 
 # Bash recipe for building across all platforms
+# Patches from https://github.com/msys2/MINGW-packages/tree/master/mingw-w64-metis
 script = raw"""
 cd $WORKSPACE/srcdir/metis-5.1.0/
+if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
+    atomic_patch -p1 $WORKSPACE/srcdir/patches/0001-mingw-w64-does-not-have-sys-resource-h.patch
+    atomic_patch -p1 $WORKSPACE/srcdir/patches/0002-mingw-w64-do-not-use-reserved-double-underscored-names.patch
+    atomic_patch -p1 $WORKSPACE/srcdir/patches/0003-WIN32-Install-RUNTIME-to-bin.patch
+    atomic_patch -p1 $WORKSPACE/srcdir/patches/0004-Fix-GKLIB_PATH-default-for-out-of-tree-builds.patch
+fi
 mkdir -p build
 cd build/
 cmake $WORKSPACE/srcdir/metis-5.1.0/ -DCMAKE_VERBOSE_MAKEFILE=1 -DGKLIB_PATH=$WORKSPACE/srcdir/metis-5.1.0/GKlib -DCMAKE_INSTALL_PREFIX=$prefix -DSHARED=1 -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
@@ -24,19 +31,7 @@ make -j${nproc} install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
